bc8cb331ba94eb362cdff50b1dcdd0dadd7c9e20
Akash Dhiwar <akashdhiwar3001@gmail.com>
2023-12-24 01:51:36 +0530

    added : cors for localhost:5173

--- Go-Shopping-backend/main.go
+++ Go-Shopping-backend/main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	// "fmt"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	// Create a new Gin router
 	router := gin.Default()
+
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:5173"} // specify the origins you want to allow
+	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE"}
+	router.Use(cors.New(config))
 
 	// Define a route
 	router.GET("/", func(c *gin.Context) {
