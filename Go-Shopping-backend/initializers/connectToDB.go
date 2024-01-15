86cc7922e0f1c2f1fdf76b94474848dfbd97c93c
Akash Dhiwar <akashdhiwar3001@gmail.com>
2024-01-15 22:33:14 +0530

    updated env file

--- Go-Shopping-backend/initializers/connectToDB.go
+++ Go-Shopping-backend/initializers/connectToDB.go
@@ -1,6 +1,7 @@
 package initializers
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -11,7 +12,15 @@ import (
 var DB *gorm.DB
 
 func ConnectToDB() {
-	dsn := os.Getenv("RAILS_DB")
+	dbUser := os.Getenv("RAILS_DATABASE_USER")
+	dbPassword := os.Getenv("RAILS_DATABASE_PASSWORD")
+	dbName := os.Getenv("RAILS_DATABASE_NAME")
+	dbHost := os.Getenv("RAILS_DATABASE_HOST")
+	dbPort := os.Getenv("RAILS_DATABASE_PORT")
+
+	// Construct DSN
+	dsn := fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%s sslmode=disable",
+		dbUser, dbPassword, dbName, dbHost, dbPort)
 
 	if dsn == "" {
 		log.Fatal("RAILS_DB environment variable is empty")
